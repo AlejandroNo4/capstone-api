4c8924c3d8247b406628df728551f6cc4ff4f8ef
Alejandro Contreras <kdta15@outlook.com>
2021-10-05 12:56:05 -0500

    Update trip.rb

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,8 +2,9 @@ class Trip < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites
 
-  validates :destination, :price, :days, :hotel, :trip_type, presence: true, length: { maximum: 50 }
-  validates :description, presence: true, length: { maximum: 400 }
+  validates :destination, :price, :days, :hotel, :trip_type, :description, presence: true, on: :create
+  validates :destination, :price, :days, :hotel, :trip_type, length: { maximum: 50 }
+  validates :description, length: { maximum: 400 }
   has_many_attached :images
 
   default_scope -> { order(created_at: :desc) }
